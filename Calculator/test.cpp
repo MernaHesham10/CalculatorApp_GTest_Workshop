0a48b60bb70758500d98fa0f3ba50c599096b36e
Merna Hesham <87778784+MernaHesham10@users.noreply.github.com>
2025-04-26 12:48:42 +0300

    Update

--- Calculator/test.cpp
+++ Calculator/test.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include <gtest/gtest.h>
-#include "E:\Workshops\GTest\Calculator\CalculatorTestSuite\Calculator.cpp"
+#include "../CalculatorTestSuite/Calculator.cpp"
 #include <tuple>
 
 // 1. Basic Add Tests
